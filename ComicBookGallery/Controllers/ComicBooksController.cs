aba694ec8698da1ba2091b4a484284c1ce35c1ea
Khryssizzle <art.of.alchemy@gmail.com>
2017-06-15 16:54:21 -0400

    Added comic book detail view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,13 +9,8 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail()
-        {   
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");    
-            }
-
-            return Content ("Hello from the comic books controller!");      
+        {
+            return View();   
         }
     }
 }
